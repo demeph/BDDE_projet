b3a28e996fd264647dfe12c4dd56fbed6360541a
Carol Couillerot <carol.couillerot@gmail.com>
2017-11-11 17:02:59 +0100

    ajout d'index

---
+++ sql/index.sql
@@ -0,0 +1,8 @@
+CREATE BITMAP INDEX bitmap_Index_Annee ON laDate ("annee");
+
+CREATE BITMAP INDEX bitmap_Index_Categorie ON TableDeFait ("categorie");
+CREATE BITMAP INDEX bitmap_Index_Categorie2 ON TableDeFait2 ("categorie"); 
+
+CREATE BITMAP INDEX bitmap_Index_Typologie ON TableDeFait ("typologie");
+CREATE BITMAP INDEX bitmap_Index_Typologie2 ON TableDeFait2 ("typologie");
+
