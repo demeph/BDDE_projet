59af6cab3c5cb9af88ba3800456eac4ddd51069b
Loïc <loic44650@users.noreply.github.com>
2017-11-13 20:56:41 +0100

    Update view.sql

--- sql/view.sql
+++ sql/view.sql
@@ -1,30 +1,29 @@
 -- Vue 1 :
 -- jointure sur les locations -> code postal, commune, departement, region
-
 CREATE MATERIALIZED VIEW detailLocation
 AS SELECT f2.codeInsee,a.commune, a.codePostale, f2.departement,f2.region
 FROM tabledefait2 f2, lesCommunes lc, adresse a
-WHERE a.codePostale = lc.codePostale and  f2.CODEINSEE = lc.codeInsee
-group by  a.commune, a.codePostale, f2.departement,f2.region;
+WHERE a.codePostale = lc.codePostale AND f2.CODEINSEE = lc.codeInsee
+GROUP BY  a.commune, a.codePostale, f2.departement,f2.region;
 
 
 -- Vue 2 :
 -- tableau des sommes
-create MATERIALIZED view somme
-as (select f2.region as Nom, sum(f.NBCHAMBRE )
-from TABLEDEFAIT f, TABLEDEFAIT2 f2
-where f.IDETABLISSEMENT = f2.IDETABLISSEMENT
-group by f2.region
-union
-select f2.departement as Nom, sum(f.NBCHAMBRE)
-from TABLEDEFAIT f, TABLEDEFAIT2 f2
-where f.IDETABLISSEMENT = f2.IDETABLISSEMENT
-group by f2.departement
-union 
-select a.commune as Nom, sum(f.NBCHAMBRE)
-from TABLEDEFAIT f, adresse a 
-where f.idAdress =  a.idAdress
-group by a.commune;);
+CREATE MATERIALIZED view somme
+AS (SELECT f2.region AS Nom, sum(f.NBCHAMBRE) AS sum
+FROM TABLEDEFAIT f, TABLEDEFAIT2 f2
+WHERE f.IDETABLISSEMENT = f2.IDETABLISSEMENT
+GROUP BY f2.region
+UNION
+SELECT f2.departement AS Nom, sum(f.NBCHAMBRE) AS sum
+FROM TABLEDEFAIT f, TABLEDEFAIT2 f2
+WHERE f.IDETABLISSEMENT = f2.IDETABLISSEMENT
+GROUP BY f2.departement
+UNION 
+SELECT a.commune AS Nom, sum(f.NBCHAMBRE) AS sum
+FROM TABLEDEFAIT f, adresse a 
+WHERE f.idAdress =  a.idAdress
+GROUP BY a.commune;);
 
 -- Vue 3 :
 -- tableau des moyennes
@@ -34,7 +33,7 @@ FROM  tableDeFait f
 GROUP BY categorie;
 
 -- Vue 4
-create materialized view densite
-asselect f2.codeINSEE, f2.population,f.superficie,(f2.population*1000/f2.superficie*10) as rapport
-from tabledefait2 f2
-group by f2.codeINsee,f2.population,f2.superficie;
+CREATE materialized view densite
+asselect f2.codeINSEE, f2.population,f.superficie,(f2.population*1000/f2.superficie*10) AS rapport
+FROM tabledefait2 f2
+GROUP BY f2.codeINsee,f2.population,f2.superficie;
