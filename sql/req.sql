06bc0134e1c278530871415e21ef27e264e2e7ea
Loïc <loic44650@users.noreply.github.com>
2017-11-13 21:03:33 +0100

    Update req.sql

--- sql/req.sql
+++ sql/req.sql
@@ -1,6 +1,5 @@
 -- Requete 1
--- Moyenne du nombre d'étoile par an et par type d'établissement,
--- par an, par type, moyenne générale
+-- Moyenne du nombre d'étoile par an et par type d'établissement, par an, par type, moyenne générale
 SELECT dt.annee, f.typologie, avg(f.classement),
 	grouping(dt.annee) as annee,
 	grouping(f.typologie) as typeEtablissement
@@ -18,10 +17,7 @@ GROUP BY GROUPING SETS ((dt.annee), (f.typologie));
 
 
 -- Requete 3
--- Fonctionne
--- Total du nombre de places par an par commune et par typo, 
--- par commune et par typo
--- avec au moins 3 étoiles
+-- Total du nombre de places par an par commune et par typo, par commune et par typo avec au moins 3 étoiles
 SELECT dt.annee, at.commune, f.typologie, sum(f.classement)
 FROM laDate dt, adresse at, tableDeFait f
 WHERE f.dateClassement = dt.idDate AND f.idAdress = at.idAdress AND classement >= 3 
@@ -29,8 +25,8 @@ GROUP BY ROLLUP (at.commune, f.typologie, dt.annee);
 
 
 -- Requete 4
--- Fonctionne 
--- Pour chaque catégorie: classement des communes par moyenne 	
+-- Pour chaque catégorie: classement des communes par moyenne
+-- version 1
 SELECT f.categorie, at.commune, avg(f.classement),
 	dense_rank() over (PARTITION BY f.categorie 
 				ORDER BY avg(f.classement) desc) rank
@@ -38,7 +34,7 @@ FROM laDate dt, adresse at, tableDeFait f
 WHERE f.idAdress = at.idAdress
 GROUP BY f.categorie, at.commune;
 
-
+-- version 2
 SELECT f.categorie, at.commune, avg(f.classement),
 	rank() over (PARTITION BY f.categorie
 				ORDER BY avg(f.classement) desc) rank
@@ -49,7 +45,6 @@ GROUP BY f.categorie, at.commune
 
 -- Requete 5
 -- Pour chaque département le classement des établissements par type de séjour
--- fonctionne
 SELECT f2.departement, f.typologie, avg(f.classement),
 	dense_rank() over (PARTITION BY etab.typeSejour ORDER BY avg(f.classement) desc) rank	
 FROM tableDeFait f, ETABLISSEMENT etab,tabledefait2 f2
@@ -58,8 +53,7 @@ GROUP BY f2.departement, f.typologie,f.CLASSEMENT,etab.typesejour;
 
 
 -- Requete 6
--- Nombre d'hôtels par habitants
--- modifie et fonctionne
+-- Nombre d'hôtels par habitants modifie et fonctionne
 SELECT lc.commune, (count(f.typologie) / f.population)
 FROM tableDeFait2 f, DETAILLOCATION lc
 WHERE f.codeInsee = lc.codeInsee and f.typologie = 'HÔTEL' and f.population != 0
@@ -68,7 +62,6 @@ GROUP BY lc.commune, f.typologie,f.population;
 
 -- Requete 7
 -- Augmentation dans le temps des places d'hébergements
--- modifie et fonctionne
 SELECT ld.annee, sum(f.nbChambre), sum(sum(f.nbChambre)) over (order by ld.annee rows unbounded preceding)
 FROM tableDeFait f, laDate ld
 WHERE f.dateClassement = ld.idDate
@@ -76,9 +69,15 @@ group by ld.annee;
 
 
 -- Requete 8
---
+-- Affiche la population moyenne des communes en fonction de la superficie des communes
 SELECT avg(rapport) 
 FROM densite;
+
+
+-- Requête 9
+--
+
+
 
 -- Requete 10
 -- Classement des regions ayant les hôtels qui ont la plus grande capacité d'accueil
