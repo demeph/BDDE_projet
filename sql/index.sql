722010b67b8589913fcc65000715c3eb1bf4d3d5
carol <carol.couillerot@gmail.com>
2017-11-13 22:13:44 +0100

    added new indexes

--- sql/index.sql
+++ sql/index.sql
@@ -1,8 +1,28 @@
 CREATE BITMAP INDEX bitmap_Index_Annee ON laDate ("annee");
 
+CREATE BITMAP INDEX bitmap_Index_StatutCommune ON lesCommunes("statut");
+
 CREATE BITMAP INDEX bitmap_Index_Categorie ON TableDeFait ("categorie");
 CREATE BITMAP INDEX bitmap_Index_Categorie2 ON TableDeFait2 ("categorie"); 
 
 CREATE BITMAP INDEX bitmap_Index_Typologie ON TableDeFait ("typologie");
 CREATE BITMAP INDEX bitmap_Index_Typologie2 ON TableDeFait2 ("typologie");
+
+CREATE BITMAP INDEX bitmap_Index_Classement ON TableDeFait ("classement");
+CREATE BITMAP INDEX bitmap_Index_Classement2 ON TableDeFait2 ("classement");
+
+CREATE BITMAP INDEX bitmap_Index_Departement ON TableDeFait2("departement");
+CREATE BITMAP INDEX bitmap_Index_Region ON TableDeFait2("region");
+
+CREATE BITMAP INDEX bij_Annee 
+	ON TableDeFait(laDate.idDate) 
+	FROM TableDeFait, laDate
+	WHERE TableDeFait.dateClassement = laDate.idDate
+;
+
+CREATE BITMAP INDEX bij_codeInsee
+	ON TableDeFait2(lesCommunes.codeInsee) 
+	FROM TableDeFait, lesCommunes
+	WHERE TableDeFait.codeInsee = lesCommunes.codeInsee
+;
 
